d3fa40824231c7fcfb079e690c032cde18772182
Jose Martinez <josealbertomartinez.vi@gmail.com>
2020-05-14 02:04:19 -0500

    updated models

--- TaskServer/app/Task.php
+++ TaskServer/app/Task.php
@@ -3,8 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Task extends Model
 {
-    //
+    use SoftDeletes; //Implementamos 
+
+    protected $guarded = [];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
